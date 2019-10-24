728593462f1f009f6f7b7556390781bf56946d06
Omar Alesharie <omar.alesharie@gmail.com>
2019-10-24 23:37:49 +0300

    CommandPattern
    Command Pattern implementation and sample

---
+++ DesignPatterns/Assets/Patterns/1-Command/Scripts/Command.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Command
+{
+    public abstract void Execute(Animator anim, bool forward);
+}
+
+public class RunFast : Command
+{
+    public override void Execute(Animator anim, bool forward)
+    {
+        if (forward)
+        {
+            anim.SetTrigger("isRunFast");
+        }
+        else
+            anim.SetTrigger("isRunFastR");
+    }
+}
+
+public class BackFlip : Command
+{
+    public override void Execute(Animator anim, bool forward)
+    {
+        if (forward)
+        {
+            anim.SetTrigger("isBackFlip");
+        }
+        else
+            anim.SetTrigger("isBackFlipR");
+    }
+}
+
+public class MagicSpell : Command
+{
+    public override void Execute(Animator anim, bool forward)
+    {
+        if (forward)
+        {
+            anim.SetTrigger("isMagicSpell");
+        }
+        else
+            anim.SetTrigger("isMagicSpellR");
+    }
+}
+
+public class UppercutJab : Command
+{
+    public override void Execute(Animator anim, bool forward)
+    {
+        if (forward)
+        {
+            anim.SetTrigger("isUppercutJab");
+        }
+        else
+            anim.SetTrigger("isUppercutJabR");
+    }
+}
+
---
+++ DesignPatterns/Assets/Patterns/1-Command/Scripts/InputHandler.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public GameObject actor;
+    Animator anim;
+    Command KeyQ, KeyW, KeyE, upArrow;
+    List<Command> oldCommands = new List<Command>();
+
+    Coroutine replayCoroutine;
+    bool shouldStartReply;
+    bool isReplaying;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        KeyQ = new BackFlip();
+        KeyW = new MagicSpell();
+        KeyE = new UppercutJab();
+        upArrow = new RunFast();
+        anim = actor.GetComponent<Animator>();
+        Camera.main.GetComponent<CameraFollow360>().player = actor.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isReplaying)
+        {
+            HandleInpput();
+        }
+        StartReplay();
+    }
+
+    void HandleInpput()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            KeyQ.Execute(anim, true);
+            oldCommands.Add(KeyQ);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            KeyW.Execute(anim, true);
+            oldCommands.Add(KeyW);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            KeyE.Execute(anim, true);
+            oldCommands.Add(KeyE);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            upArrow.Execute(anim, true);
+            oldCommands.Add(upArrow);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shouldStartReply = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            UndoLastCommand();
+        }
+    }
+
+    void UndoLastCommand()
+    {
+        if (oldCommands.Count > 0)
+        {
+            Command c = oldCommands[oldCommands.Count - 1];
+            c.Execute(anim, false);
+            oldCommands.RemoveAt(oldCommands.Count - 1);
+        }
+    }
+
+    void StartReplay()
+    {
+        if (shouldStartReply && oldCommands.Count > 0)
+        {
+            shouldStartReply = false;
+            if (replayCoroutine != null)
+            {
+                StopCoroutine(replayCoroutine);
+            }
+            replayCoroutine = StartCoroutine(ReplayCommands());
+        }
+    }
+
+    IEnumerator ReplayCommands()
+    {
+        isReplaying = true;
+
+        for (int i = 0; i < oldCommands.Count; i++)
+        {
+            oldCommands[i].Execute(anim, true);
+            yield return new WaitForSeconds(1f);
+        }
+
+        isReplaying = false;
+    }
+}
